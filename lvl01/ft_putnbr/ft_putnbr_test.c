8cb565035219283cd369723d944c964e189c1166
Mike Meyer <mfmeyer301@gmail.com>
2019-02-26 20:43:24 +0200

    adding ft_putnbr

---
+++ lvl01/ft_putnbr/ft_putnbr_test.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_putnbr(int nb)
+{
+	if (nb < 0)
+	{
+		nb *= -1;
+		ft_putchar('-');
+	}
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+	}
+	ft_putchar(nb % 10 + 48);
+}
+
+int main(void)
+{
+	ft_putnbr(127364);
+	return (0);
+}
