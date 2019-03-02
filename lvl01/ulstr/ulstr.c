33f759140987e08375ad4e42f8b8b316153a69e6
Mike Meyer <mfmeyer301@gmail.com>
2019-03-02 14:39:04 +0000

    ulstr.c edited online with Bitbucket

--- lvl01/ulstr/ulstr.c
+++ lvl01/ulstr/ulstr.c
@@ -5,39 +5,37 @@ void ft_putchar(char c)
 	write(1,&c,1);
 }
 
-void ft_putstring(char *c)
+void ft_putstring(char *str)
 {
-	int y;
+	int i;
 	
-	y=0;
-	while (c[y] != '\0')
+	i = 0;
+	while (str[i] != '\0')
 	{
-		ft_putchar(c[y]);
-		y++;
+		ft_putchar(str[i]);
+		i++;
 	}
 }
 int main(int argc, char **argv)
 {
-	int x;
 	int y;
 
-	x = 1;
 	y = 0;
 	if (argc != 2)
 	{
 		ft_putchar('\n');
 		return(0);
 	}
-	while (argv[x][y] != '\0')
+	while (argv[1][y] != '\0')
 	{
-		if (argv[x][y] >= 'A' && argv[x][y] <= 'Z')
+		if (argv[1][y] >= 'A' && argv[1][y] <= 'Z')
 		{
-			argv[x][y] += 32;
+			argv[1][y] += 32;
 			y++;
 		}
-		else if (argv[x][y] >= 'a' && argv[x][y] <='z')
+		else if (argv[1][y] >= 'a' && argv[1][y] <='z')
 		{
-			argv[x][y] -= 32;		
+			argv[1][y] -= 32;		
 			y++;
 		}
 		else
@@ -45,7 +43,7 @@ int main(int argc, char **argv)
 			y++;
 		}
 	}
-	ft_putstring(argv[x]);
+	ft_putstring(argv[1]);
 	ft_putchar('\n');
 	return(0);
 }
