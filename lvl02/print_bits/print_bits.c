771569d6e1d62e268387adf0533d3fc2664b6ccc
Mike Meyer <mfmeyer301@gmail.com>
2019-03-03 14:26:14 +0000

    print_bits.c edited because Shaun's code is to complex. HE STOLE IT!!!!

--- lvl02/print_bits/print_bits.c
+++ lvl02/print_bits/print_bits.c
@@ -1,65 +1,27 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void	print_bits(unsigned char octet)
+void print_bits(unsigned char octet)
 {
-	int		i;
+  int i = 7;
+  char bits[9];
 
-	i = 128;
-	while (i > 0)
-	{
-		if (octet < i)
-		{
-			write(1, "0", 1);
-			i /= 2;
-		}
-		else
-		{
-			write(1, "1", 1);
-			octet -= i;
-			i /= 2;
-		}
-	}
-}
-
-void	rev_bits(unsigned char octet)
-{
-	int		i;
-	int		c;
-	char	arr[9];
-
-	i = 128;
-	c = 0;
-	while (i > 0)
-	{
-		if (octet < i)
-		{
-			arr[c] = '0';
-			i /= 2;
-		}
-		else
-		{
-			arr[c] = '1';
-			octet -= i;
-			i /= 2;
-		}
-		c++;
-	}
-	while (c-- >= 0)
-	{
-		write(1, &arr[c],1);
-	}
+  while (i >= 0)
+  {
+             bits[i] = (octet % 2) + '0';
+             octet = octet / 2;
+             i--;
+  }
+  bits[8] = '\0';
+  ft_putstr(bits);
 }
 
 int main(int ac, char **av)
 {
 	char test = av[2][0];
-	if (ac == 3)
+	if (ac == 2)
 	{
-		if (av[1][0] == '1')
-			print_bits(test);
-		if (av[1][0] == '2')
-			rev_bits(test);
+		print_bits(test);
 	}
 	write(1, "\n", 1);
 	return (0);
