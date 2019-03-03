1e1b1483093930d04babafc73b46500a0df5e5df
Mike Meyer <mfmeyer301@gmail.com>
2019-03-03 13:38:35 +0000

    ft_strcmp.c edited because people

--- lvl02/ft_strcmp/ft_strcmp.c
+++ lvl02/ft_strcmp/ft_strcmp.c
@@ -16,10 +16,10 @@ int		ft_strcmp(char *s1, char *s2)
 	int s1_total;
 	int s2_total;
 
-	count = -1;
+	count = 0;
 	s1_total = 0;
 	s2_total = 0;
-	while (s1[++count] != '\0')
+	while (s1[count] != '\0')
 	{
 		if (s1[count] != s2[count])
 		{
@@ -27,6 +27,7 @@ int		ft_strcmp(char *s1, char *s2)
 			s2_total = s2[count];
 			break ;
 		}
+		count++;
 	}
 	return (s1_total - s2_total);
 }
